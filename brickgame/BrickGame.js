727089a00013b10b0dd9af65fb4a8a4cd23c2f36
DB <rkdrbgks22@ks.ac.kr>
2019-10-04 10:31:38 +0900

    코드의 함수를 분석하여 주석을 달았음

--- brickgame/BrickGame.js
+++ brickgame/BrickGame.js
@@ -26,7 +26,7 @@ for(var c=0; c<brickColumnCount; c++) {
     for(var r=0; r<brickRowCount; r++) {
         bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
-}
+} // 변수에 지정해둔 벽돌의 행렬 값을 통해 벽돌 행렬을 구현
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -49,6 +49,7 @@ function keyUpHandler(e) {
         leftPressed = false;
     }
 }
+// 마우스의 눌러짐을 감지하여 true, false로 값을 변경해주는 부분
 
 function mouseMoveHandler(e) {
     var relativeX = e.clientX - canvas.offsetLeft;
@@ -73,7 +74,7 @@ function collisionDetection() {
             }
         }
     }
-}
+} // 공과 벽돌의 충돌을 감지하고 벽돌을 부수며 모든 벽돌이 파괴되었을 경우 클리어 문구를 출력하는 부분
 
 function drawBall() {
     ctx.beginPath();
@@ -81,14 +82,16 @@ function drawBall() {
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
-}
+} // 벽돌을 부숴줄 공을 생성
+
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
-}
+} // 튕겨져나온 공을 받아칠 패들을 생성
+
 function drawBricks() {
     for(var c=0; c<brickColumnCount; c++) {
         for(var r=0; r<brickRowCount; r++) {
@@ -105,17 +108,19 @@ function drawBricks() {
             }
         }
     }
-}
+} // 앞서 만들어둔 벽돌 행렬을 토대로 벽돌을 그려냄
+
 function drawScore() {
     ctx.font = "16px Arial";
     ctx.fillStyle = "#0095DD";
     ctx.fillText("Score: "+score, 8, 20);
-}
+} // 화면에 벽돌을 파괴해서 얻은 점수를 표시
+
 function drawLives() {
     ctx.font = "16px Arial";
     ctx.fillStyle = "#0095DD";
     ctx.fillText("Lives: "+lives, canvas.width-65, 20);
-}
+} // 화면에 플레이어의 남은 목숨의 수를 표시
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -125,6 +130,7 @@ function draw() {
     drawScore();
     drawLives();
     collisionDetection();
+    // 앞서 생성한 함수들을 호출
 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -132,6 +138,7 @@ function draw() {
     if(y + dy < ballRadius) {
         dy = -dy;
     }
+    // 공이 벽을 닿으면 반대방향으로 이동하도록 좌표를 수정
     else if(y + dy > canvas.height-ballRadius) {
         if(x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
@@ -151,17 +158,16 @@ function draw() {
             }
         }
     }
-
+    // 공이 바닥에 닿으면 게임오버
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
         paddleX += 7;
     }
     else if(leftPressed && paddleX > 0) {
         paddleX -= 7;
-    }
-
+    } // 키보드 입력으로 패들을 움직일 경우 패들의 속도를 지정하는 부분
     x += dx;
     y += dy;
-    requestAnimationFrame(draw);
+    requestAnimationFrame(draw); // 고정 프레임 속도보다 게임을 더 잘 렌더링 할 수 있도록 설정
 }
 
-draw();
+draw(); // draw 함수를 실행하여 실제 게임이 동작 되도록 함
